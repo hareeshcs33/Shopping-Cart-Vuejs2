d5e477d0eb88893c79eac4d7106f375dc00120b6
hareeshcs33 <hareeshcs33@gmail.com>
2021-07-20 17:48:45 +0530

    tabs added

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,5 +1,48 @@
 <template>
   <div class="home">
     <h1>about</h1>
+    <div class="d-flex">
+      <breadcrumb class="row justify-content-center m-4" :crumbs="crumbs" @crumbSelected="crumbSelected" />
+    </div>
+    <div class="mx-5">
+      <tabs-nav :tabs="['Home','settings','Profile']" :selected="selected" @selected="tabSetSelected">
+        <tab :isSelected="selected === 'Home'">
+          <p>home tab</p>
+        </tab>
+        <tab :isSelected="selected === 'settings'">
+          <p>settings tab</p>
+        </tab>
+        <tab :isSelected="selected === 'Profile'">
+          <p>Profile tab</p>
+        </tab>
+      </tabs-nav>
+    </div>
   </div>
 </template>
+<script>
+import Breadcrumb from '../modules/breadcrumb/components/breadcrumb.vue';
+import TabsNav from '../modules/tabs/components/tabs-nav.vue';
+import Tab from '../modules/tabs/components/tab.vue';
+
+export default {
+  data(){
+    return {
+      crumbs: ["userList", "user", "userInfo"],
+      selected: "Home"
+    }
+  },
+  components: {
+    Breadcrumb,
+    TabsNav,
+    Tab
+  },
+  methods: {
+    crumbSelected(crumb){
+      console.log(crumb);
+    },
+    tabSetSelected(tab){
+      this.selected = tab;
+    }
+  }
+}
+</script>
---
+++ src/modules/breadcrumb/components/breadcrumb.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item d-flex align-items-center" v-for="(crumb, index) in crumbs" :key="index">
+                <button class="btn btn-link" :class="{ disabled: isLast(index) }" @click="crumbSelected(crumb)">{{crumb}}</button>
+            </li>
+        </ol>
+    </nav>
+</template>
+<script>
+export default {
+    props: {
+        crumbs: {
+            type: Array,
+            required: true
+        }
+    },
+    methods: {
+        isLast(index){
+            return index === this.crumbs.length - 1;
+        },
+        crumbSelected(crumb) {
+            this.$emit("crumbSelected", crumb);
+        }
+    }
+}
+</script>
+<style scoped>
+.breadcrumb {
+    background-color: #fff;
+    border: 1px solid rgba(0, 0, 0, 0.125);
+    border-radius: 3px;
+}
+</style>
